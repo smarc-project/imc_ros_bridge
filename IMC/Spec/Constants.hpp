0d44fd7dd6e405f39cb054e05255f7ca27681bbb
Ricardo Martins <rasm@oceanscan-mst.com>
2018-06-05 14:32:20 +0100

    IMC v5.4.18 C++ Bindings.

---
+++ IMC/Spec/Constants.hpp
@@ -0,0 +1,41 @@
+//***************************************************************************
+// Copyright 2017 OceanScan - Marine Systems & Technology, Lda.             *
+//***************************************************************************
+// Licensed under the Apache License, Version 2.0 (the "License");          *
+// you may not use this file except in compliance with the License.         *
+// You may obtain a copy of the License at                                  *
+//                                                                          *
+// http://www.apache.org/licenses/LICENSE-2.0                               *
+//                                                                          *
+// Unless required by applicable law or agreed to in writing, software      *
+// distributed under the License is distributed on an "AS IS" BASIS,        *
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+// See the License for the specific language governing permissions and      *
+// limitations under the License.                                           *
+//***************************************************************************
+// Author: Ricardo Martins                                                  *
+//***************************************************************************
+// Automatically generated.                                                 *
+//***************************************************************************
+// IMC XML MD5: 6ce3702ce15845d52f703d618b715907                            *
+//***************************************************************************
+
+#ifndef IMC_CONSTANTS_HPP_INCLUDED_
+#define IMC_CONSTANTS_HPP_INCLUDED_
+
+//! IMC version string.
+#define IMC_CONST_VERSION "5.4.18"
+//! Git repository information.
+#define IMC_CONST_GIT_INFO "2018-01-08 cd7f37a  (HEAD, tag: imc-5.4.18)"
+//! MD5 sum of XML specification file.
+#define IMC_CONST_MD5 "6ce3702ce15845d52f703d618b715907"
+//! Synchronization number.
+#define IMC_CONST_SYNC 0xFE54
+//! Reversed synchronization number.
+#define IMC_CONST_SYNC_REV 0x54FE
+//! Size of the header in bytes.
+#define IMC_CONST_HEADER_SIZE 20
+//! Size of the footer in bytes.
+#define IMC_CONST_FOOTER_SIZE 2
+
+#endif
